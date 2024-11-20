ff0a13448444ffbf25717d88c276a339e4d6d09b
n.vasilev <hackyourw@gmail.com>
2024-11-20 03:38:14 +0300

    Обновлённый вид заявки на работы, но ещё без функционала

--- lib/Views/MainScreen/Meleoration_object/System in object/Function in object/Applications for work/Form/enter_job_application_form.dart
+++ lib/Views/MainScreen/Meleoration_object/System in object/Function in object/Applications for work/Form/enter_job_application_form.dart
@@ -1,6 +1,12 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:intl/intl.dart';
 
 import '../../../../../../../Models/my_arguments.dart';
+import '../../../../../../../UI-kit/Widgets/show_snack_bar.dart';
 import '../list_enter_job_application.dart';
 
 class EnterJobApplicationForm extends StatefulWidget {
@@ -9,12 +15,16 @@ class EnterJobApplicationForm extends StatefulWidget {
 }
 
 class _EnterJobApplicationFormState extends State<EnterJobApplicationForm> {
+  final ShowSnackBar showSnackBar = ShowSnackBar();
+
   Application? application; // Объект заявки
   late TextEditingController _descriptionController; // Контроллер для описания
+  DateTimeRange? selectedDateRange; // Диапазон выбранных дат
   DateTime? _startJobApplication; // Начальная дата работы
   DateTime? _endJobApplication; // Конечная дата работы
   bool _initialized = false; // Флаг для отслеживания инициализации
   String nameObject = 'Название объекта отсутствует';
+  final List<File> _attachedFiles = []; //список прикреплённых файлов
 
   @override
   void initState() {
@@ -26,34 +36,37 @@ class _EnterJobApplicationFormState extends State<EnterJobApplicationForm> {
   void didChangeDependencies() {
     super.didChangeDependencies();
     if (!_initialized) {
-      final MyArguments args = ModalRoute.of(context)?.settings.arguments as MyArguments;
+      final MyArguments args = ModalRoute
+          .of(context)
+          ?.settings
+          .arguments as MyArguments;
 
       application = args.param3; // Получаем объект Application
       if (application != null) {
         _descriptionController.text = application!.description;
-        _startJobApplication = DateTime.parse(application!.startJobDate);
-        _endJobApplication = DateTime.parse(application!.endJobDate);
+        // Устанавливаем начальный диапазон если они известны
+        selectedDateRange = DateTimeRange(
+          start: DateTime.parse(application!.startJobDate),
+          end: DateTime.parse(application!.endJobDate),
+        );
         _initialized = true;
         nameObject = args.param4;
       }
     }
   }
 
-  Future<void> _selectDate(BuildContext context, bool isStartDate) async {
-    final DateTime? picked = await showDatePicker(
+  Future<void> _selectDateRange(BuildContext context) async {
+    DateTimeRange? picked = await showDateRangePicker(
       context: context,
-      initialDate: isStartDate ? (_startJobApplication ?? DateTime.now()) : (_endJobApplication ?? DateTime.now()),
-      firstDate: DateTime(2000),
+      initialDateRange: selectedDateRange,
+      firstDate: DateTime(2024),
       lastDate: DateTime(2101),
     );
 
     if (picked != null) {
       setState(() {
-        if (isStartDate) {
-          _startJobApplication = picked;
-        } else {
-          _endJobApplication = picked;
-        }
+        selectedDateRange = picked;
+// Здесь вы можете обновлять ставя значение в другие переменные
       });
     }
   }
@@ -69,71 +82,262 @@ class _EnterJobApplicationFormState extends State<EnterJobApplicationForm> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(application == null ? 'Создать заявку' : 'Редактировать заявку'),
+        title: Text(
+            application == null ? 'Создать заявку' : 'Редактировать заявку'),
       ),
-      body: Padding(
+      body: SingleChildScrollView( child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             TextField(
-              controller: TextEditingController(text: application?.status ?? 'В проекте'),
-              decoration: InputDecoration(labelText: 'Статус'),
+              controller: TextEditingController(
+                  text: application?.status ?? 'В проекте'),
+              decoration: const InputDecoration(
+                labelText: 'Статус', border: InputBorder.none,),
+              style: const TextStyle(fontWeight: FontWeight.bold),
               readOnly: true,
             ),
             SizedBox(height: 8),
             TextField(
-              controller: TextEditingController(text: application?.owner ?? 'Неизвестный автор'),
-              decoration: InputDecoration(labelText: 'Автор'),
+              controller: TextEditingController(
+                  text: application?.owner ?? 'Неизвестный автор'),
+              decoration: InputDecoration(
+                labelText: 'Автор', border: InputBorder.none,),
+              style: const TextStyle(fontWeight: FontWeight.bold),
               readOnly: true,
             ),
             SizedBox(height: 8),
             TextField(
               controller: TextEditingController(text: nameObject),
-              decoration: InputDecoration(labelText: 'Мелиоративный объект'),
+              decoration: InputDecoration(
+                labelText: 'Мелиоративный объект', border: InputBorder.none,),
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
               readOnly: true,
+              maxLines: 2,
             ),
             SizedBox(height: 8),
             TextField(
               controller: TextEditingController(text: '30Ф-ОРО-0001-ООС-001'),
-              decoration: InputDecoration(labelText: 'ЕИН объекта'),
+              decoration: InputDecoration(
+                labelText: 'ЕИН объекта', border: InputBorder.none,),
+              style: const TextStyle(fontWeight: FontWeight.bold),
               readOnly: true,
             ),
             SizedBox(height: 8),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('Начальная дата: ${_startJobApplication != null ? _startJobApplication!.toLocal().toString().split(' ')[0] : 'Не выбрано'}'),
-                TextButton(
-                  onPressed: () => _selectDate(context, true),
-                  child: Text('Выбрать'),
+            // Отображение диапазона дат
+            GestureDetector(
+              onTap: () => _selectDateRange(context),
+              child: InputDecorator(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Выбранный диапазон дат',
                 ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('Конечная дата: ${_endJobApplication != null ? _endJobApplication!.toLocal().toString().split(' ')[0] : 'Не выбрано'}'),
-                TextButton(
-                  onPressed: () => _selectDate(context, false),
-                  child: Text('Выбрать'),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(selectedDateRange != null
+                        ? '${DateFormat('dd.MM.yyyy').format(
+                        selectedDateRange!.start)} - ${DateFormat('dd.MM.yyyy')
+                        .format(selectedDateRange!.end)}'
+                        : 'Выберите диапазон'),
+                    Icon(Icons.calendar_today, color: Colors.grey),
+                  ],
                 ),
-              ],
+              ),
             ),
             SizedBox(height: 16),
             TextField(
-              controller: _descriptionController, // Используем контроллер для описания
-              decoration: InputDecoration(labelText: 'Описание'),
-              maxLines: 3,
+              controller: _descriptionController,
+              maxLines: 5,
+              decoration: const InputDecoration(
+                labelText: 'Описание',
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                      color: Color.fromARGB(255, 0, 78, 167)),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey),
+                ),
+              ),
             ),
             SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: (){},
-              child: Text('Сохранить заявку'),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: const Color.fromARGB(255, 0, 78, 167),
+                  backgroundColor: Colors.white,
+                  side: const BorderSide(
+                      color: Color.fromARGB(255, 0, 78, 167), width: 2.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                onPressed: _attachFile,
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.download),
+                    Text('Прикрепить файл'),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: _attachedFiles.length,
+              itemBuilder: (context, index) {
+                return Row(
+                  children: [
+                    Expanded(
+                      child: ListTile(
+                        title: Row(
+                          children: [
+                            const Icon(Icons.file_open_rounded,
+                                color: Color.fromARGB(255, 0, 78, 167)),
+                            const SizedBox(width: 8),
+                            Expanded(
+                              child: Text(
+                                '${truncateText(_attachedFiles[index].path
+                                    .split('/')
+                                    .last, 25)}',
+                                style: const TextStyle(
+                                    color: Color.fromARGB(255, 0, 78, 167)),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ],
+                        ),
+                        trailing: IconButton(
+                          icon: const Icon(Icons.close,
+                              color: Color.fromARGB(255, 0, 78, 167)),
+                          onPressed: () => _removeFile(index),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.white,
+                  side: const BorderSide(color: Color.fromARGB(255, 0, 78, 167), width: 2.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text('Сохранить проект', style: TextStyle(color: Color.fromARGB(255, 0, 78, 167)),),
+              ),
+            ),
+            if (application?.status == 'В проекте') // Проверка статуса
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.red,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Text('Удалить проект'),
+                ),
+              ),
+            const SizedBox(height: 4),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: const Color.fromARGB(255, 0, 78, 167),
+                  side: const BorderSide(color: Color.fromARGB(255, 0, 78, 167), width: 2.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text('Отправить'),
+              ),
             ),
           ],
         ),
       ),
+      ),
     );
   }
+
+  Future<void> _attachFile() async {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text('Выберите способ прикрепления файла', style: TextStyle(fontSize: 18)),
+              const SizedBox(height: 16),
+              TextButton(
+                onPressed: () async {
+                  final ImagePicker _picker = ImagePicker(); // Создаем экземпляр ImagePicker
+                  final XFile? photo = await _picker.pickImage(source: ImageSource.camera);
+                  if (photo != null) {
+                    setState(() {
+                      _attachedFiles.add(File(photo.path));
+                    });
+                  }
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Сделать фото'),
+              ),
+              TextButton(
+                onPressed: () async {
+                  FilePickerResult? result = await FilePicker.platform.pickFiles(allowMultiple: false, type: FileType.any);
+                  if (result != null && result.files.isNotEmpty) {
+                    setState(() {
+                      _attachedFiles.add(File(result.files.single.path!));
+                    });
+                  }
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Выбрать файл'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Отмена'),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void _removeFile(int index) {
+    setState(() {
+      _attachedFiles.removeAt(index);
+    });
+  }
+
+  String truncateText(String text, int maxLength) {
+    if (text.length > maxLength) {
+      return '${text.substring(0, maxLength)}...';
+    }
+    return text;
+  }
 }
+
+
+
+
